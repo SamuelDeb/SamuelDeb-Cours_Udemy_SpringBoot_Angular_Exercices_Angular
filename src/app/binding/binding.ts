dc6c9623d80accc63c55ee484265b374ab647b44
SamuelDeb <samuel.debaer@gmail.com>
2025-09-01 14:03:07 +0200

    ajout de la propriété standalone : true dans @Component

--- src/app/binding/binding.ts
+++ src/app/binding/binding.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-binding',
+  standalone: true,
   imports: [],
   templateUrl: './binding.html',
   styles: ``
 })
-export class Binding {
+export class Binding implements OnInit {
+
+  titre : string = 'demo du Data Binding Interpolation';
+  constructor() { }
+  ngOnInit(): void {
+  }
 
 }
